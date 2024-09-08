61fc1ba1d81edc7ef8d0dc7e17bf541411f89d8c
rishavvajpayee <reshavvajpayee@gmail.com>
2024-09-08 23:52:54 +0530

    added channels for concurrency

--- main.go
+++ main.go
@@ -15,27 +15,42 @@ func main() {
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func handleAny(context echo.Context) error {
-	targetPostfixURL := context.Request().URL.String()
+func handleAny(c echo.Context) error {
+	targetPostfixURL := c.Request().URL.String()
 	targetURL := string(config.AppConfig.ProxyTargetUrl) + targetPostfixURL
-	req, err := http.NewRequest(context.Request().Method, targetURL, context.Request().Body)
-	if err != nil {
-		return context.String(http.StatusBadRequest, "Bad Request")
-	}
-	req.Header = context.Request().Header
-	println("REQUEST URL: %s", targetURL)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return context.String(http.StatusBadGateway, "Failed to connect to destination server")
-	}
-	defer resp.Body.Close()
 
-	for k, v := range resp.Header {
-		context.Response().Header().Set(k, v[0])
+	req, err := http.NewRequest(c.Request().Method, targetURL, c.Request().Body)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Bad Request")
 	}
-	context.Response().WriteHeader(resp.StatusCode)
+	req.Header = c.Request().Header
 
-	_, err = io.Copy(context.Response().Writer, resp.Body)
-	return err
+	responseChannel := make(chan *http.Response)
+	errorChannel := make(chan error)
+
+	go func() {
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			errorChannel <- err
+			return
+		}
+		responseChannel <- resp
+	}()
+
+	select {
+	case resp := <-responseChannel:
+		defer resp.Body.Close()
+
+		for k, v := range resp.Header {
+			c.Response().Header().Set(k, v[0])
+		}
+		c.Response().WriteHeader(resp.StatusCode)
+
+		_, err := io.Copy(c.Response().Writer, resp.Body)
+		return err
+
+	case err := <-errorChannel:
+		return c.String(http.StatusBadGateway, "Failed to connect to destination server: "+err.Error())
+	}
 }
