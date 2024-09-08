82a5a46bb5b1091a0683872d0eb190a1c9faa0f6
rishavvajpayee <reshavvajpayee@gmail.com>
2024-09-09 00:46:14 +0530

    removed goroutines ( already handled by echo ) added timeout

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"io"
 	"net/http"
 	config "proxyserver/pkg"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
+var httpClient *http.Client
+
 func main() {
 	config.LoadConfig()
+	httpClient = &http.Client{
+		Timeout: 10 * time.Second,
+	}
 	e := echo.New()
 	e.Any("/*", handleAny)
 	e.Logger.Fatal(e.Start(":8080"))
@@ -17,40 +23,25 @@ func main() {
 
 func handleAny(c echo.Context) error {
 	targetPostfixURL := c.Request().URL.String()
-	targetURL := string(config.AppConfig.ProxyTargetUrl) + targetPostfixURL
+	targetURL := config.AppConfig.ProxyTargetUrl + targetPostfixURL
 
 	req, err := http.NewRequest(c.Request().Method, targetURL, c.Request().Body)
 	if err != nil {
-		return c.String(http.StatusBadRequest, "Bad Request")
+		return c.String(http.StatusBadRequest, "Bad Request: "+err.Error())
 	}
-	req.Header = c.Request().Header
-
-	responseChannel := make(chan *http.Response)
-	errorChannel := make(chan error)
-
-	go func() {
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			errorChannel <- err
-			return
-		}
-		responseChannel <- resp
-	}()
-
-	select {
-	case resp := <-responseChannel:
-		defer resp.Body.Close()
-
-		for k, v := range resp.Header {
-			c.Response().Header().Set(k, v[0])
-		}
-		c.Response().WriteHeader(resp.StatusCode)
-
-		_, err := io.Copy(c.Response().Writer, resp.Body)
-		return err
-
-	case err := <-errorChannel:
+	req.Header = c.Request().Header.Clone()
+	resp, err := httpClient.Do(req)
+	if err != nil {
 		return c.String(http.StatusBadGateway, "Failed to connect to destination server: "+err.Error())
 	}
+	defer resp.Body.Close()
+
+	for k, v := range resp.Header {
+		for _, value := range v {
+			c.Response().Header().Add(k, value)
+		}
+	}
+	c.Response().WriteHeader(resp.StatusCode)
+	_, err = io.Copy(c.Response().Writer, resp.Body)
+	return err
 }
